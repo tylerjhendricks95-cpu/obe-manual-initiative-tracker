3991b8810b0595db21ebf0a53c528712900394c3
tylerjhendricks95-cpu <tyler.j.hendricks95@gmail.com>
2026-07-31 01:51:40 -0400

    Configure base path for GitHub Pages
    Set base path for GitHub Pages to load assets correctly.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "./", // ENSURES GITHUB PAGES LOADS CSS & JS RELATIVE PATHS CORRECTLY
   build: {
-    target: "esnext", // THIS TELLS ESBUILD TO ALLOW TOP-LEVEL AWAIT
+    target: "esnext",
   },
   server: {
     cors: true,
